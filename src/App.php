a7acb68addf34f8d428fc3fa77c3e0c7ed0ad594
Michael Phillips <michaeljoelphillips@gmail.com>
2018-03-10 13:18:57 -0600

    Stash commit.

--- src/App.php
+++ src/App.php
@@ -1,5 +1,11 @@
 <?php
 
+use BotMan\BotMan\BotMan;
+use BotMan\BotMan\BotManFactory;
+use BotMan\BotMan\Drivers\DriverManager;
+use BotMan\Drivers\Slack\SlackDriver;
+use Dotenv\Dotenv;
+
 namespace DockerForProduction;
 
 /**
@@ -7,8 +13,24 @@ namespace DockerForProduction;
  */
 class App
 {
-    public static function main() : void
+    public static function start() : void
     {
-        echo "Welcome to Dallas PHP!";
+        $this->loadEnvironment();
+
+        DriverManager::loadDriver(SlackDriver::class);
+
+        $config = [
+            'slack' => [
+                'token' => getenv('SLACK_TOKEN')
+            ]
+        ];
+
+        BotManFactory::create($config);
+    }
+
+    private static function loadEnvironment()
+    {
+        $dotenv = new Dotenv(__DIR__);
+        $dotenv->load();
     }
 }
