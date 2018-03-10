631c496993b7d1e196f42e083a3a1237bfebe36e
Michael Phillips <michaeljoelphillips@gmail.com>
2018-03-10 17:30:58 -0600

    Add a chat bot.

--- src/App.php
+++ src/App.php
@@ -1,22 +1,44 @@
 <?php
 
+namespace DockerForProduction;
+
 use BotMan\BotMan\BotMan;
 use BotMan\BotMan\BotManFactory;
 use BotMan\BotMan\Drivers\DriverManager;
 use BotMan\Drivers\Slack\SlackDriver;
+use BotMan\BotMan\Cache\SymfonyCache;
 use Dotenv\Dotenv;
-
-namespace DockerForProduction;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 
 /**
  * @author Michael Phillips <michaeljoelphillips@gmail.com>
  */
 class App
 {
-    public static function start() : void
+    public function start() : void
     {
         $this->loadEnvironment();
+        $botman = $this->createBotMan();
 
+        $botman->hears('Can you hear me\?', function (BotMan $bot) {
+            $bot->reply('Loud and clear!');
+        });
+
+        $botman->hears('Say hi, BotMan!', function (BotMan $bot) {
+            $bot->reply('Welcome to Dallas PHP!');
+        });
+
+        $botman->listen();
+    }
+
+    private function loadEnvironment()
+    {
+        $dotenv = new Dotenv(__DIR__ . '/../');
+        $dotenv->load();
+    }
+
+    private function createBotMan() : BotMan
+    {
         DriverManager::loadDriver(SlackDriver::class);
 
         $config = [
@@ -25,12 +47,9 @@ class App
             ]
         ];
 
-        BotManFactory::create($config);
-    }
+        $adapter = new FilesystemAdapter();
+        $botman = BotManFactory::create($config, new SymfonyCache($adapter));
 
-    private static function loadEnvironment()
-    {
-        $dotenv = new Dotenv(__DIR__);
-        $dotenv->load();
+        return $botman;
     }
 }
